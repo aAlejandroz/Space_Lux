58c0111bf8643b2cc5e49215b4a782669055ce91
TheZebull <nicholas.ghansyam@gmail.com>
2018-11-28 23:57:00 -0600

    Added Control when the game starts

---
+++ Assets/Scripts/UI/Controls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+
+    public GameObject CrossHair;
+    public GameObject ControlHUD;
+    
+    
+
+	// Use this for initialization
+	void Start () {
+        
+        Cursor.visible = true;
+        CrossHair.SetActive(false);
+        ControlHUD.SetActive(true);
+        Time.timeScale = 0f;
+    }
+	
+	// Update is called once per frame
+	public void PlayGame()
+    {
+        
+        CrossHair.SetActive(true);
+        Cursor.visible = false;
+        ControlHUD.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+}
